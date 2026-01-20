222590661e09b3ac99fbd2df90f99040e8ce0cd2
mengJiuXiang <571343428@qq.com>
2026-01-20 11:12:19 +0800

    统计

--- jj易教何子全python/project/20260124/result.py
+++ jj易教何子全python/project/20260124/result.py
@@ -7,17 +7,58 @@ import xlwings as xw
 import shutil
 
 shutil.copy('合同台账.xlsx', '合同台账_1到12月统计.xlsx')
-appMain = xw.App(visible=False)  # 不显示Excel界面
-wbMain = appMain.books.open('合同台账_1到12月统计.xlsx')
-wsMain = wbMain.sheets[0]
-wsMain.name = f"{1}月"
+appChild = xw.App(visible=False)  # 不显示Excel界面
+wbChild = appChild.books.open('合同台账_1到12月统计.xlsx')
+wsChild = wbChild.sheets[0]
+wsChild.name = f"{1}月"
 
 for i in range(11):
-    wsMain.copy(name=f"{i+2}月")  # 给新工作表命名
+    wsChild.copy(name=f"{i+2}月")  # 给新工作表命名
 
-wbMain.save('合同台账_1到12月统计.xlsx')
-wbMain.close()
-appMain.quit()
+wbChild.save('合同台账_1到12月统计.xlsx')
+wbChild.close()
+appChild.quit()
+
+childColumn={
+    '档口号':2,
+    '起租日':13,
+    '到期日':14,
+    '租金':16
+}
+
+has31=[1,3,5,7,8,10,12]
+has30=[4,6,9,11]
+current2Days=28
+
+dfReadChild = pd.read_excel(
+    '合同台账_1到12月统计.xlsx', 
+    sheet_name='12月',
+    header=[0,1,2,3],
+)
+
+dfChildList=dfReadChild.values.tolist()
+appChild = xw.App(visible=False)  # 不显示Excel界面
+wbChild = appChild.books.open('合同台账_1到12月统计.xlsx')
+for currentMonthFrom0 in range(12):
+    currentMonthFrom1=currentMonthFrom0+1
+    endDay=current2Days
+    if currentMonthFrom1 in has31:
+        endDay=31
+    elif currentMonthFrom1 in has30:
+        endDay=30
+    ts1 = pd.Timestamp(f'2026-{currentMonthFrom1}-01 00:00:00')
+    ts2 = pd.Timestamp(f'2026-{currentMonthFrom1}-{endDay} 23:59:59')
+    for childIndex,childItem in enumerate(dfChildList):
+        if (not (ts2<childItem[childColumn['起租日']] or childItem[childColumn['到期日']]<ts1)) and (not pd.isna(childItem[childColumn['租金']])):
+            print(f'这一行不用动，满足在租在{currentMonthFrom1}月且有租金',childItem,ts1,ts2,childItem[childColumn['到期日']],childItem[childColumn['起租日']],(childItem[childColumn['到期日']]>=ts1),(childItem[childColumn['起租日']]<=ts2))
+        else:
+            wsChild = wbChild.sheets[currentMonthFrom0]
+            wsChild[4+childIndex, childColumn['租金']].value=0
+
+wbChild.save('合同台账_1到12月统计.xlsx')
+wbChild.close()
+appChild.quit()
+
 
 # dfReadMain = pd.read_excel(
 #     '鞋城2026.1月份租金).xlsx', 
