77c984589acf17ce8c3559f27c7e3946a805175a
mengjiuxiang <571343428@qq.com>
2026-02-14 12:58:33 +0800

    对比

---
+++ jj易教何子全python/project/20260214/result.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from datetime import datetime
+import json
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+import xlwings as xw
+import shutil
+
+dfReadLeft = pd.read_excel(
+    '121 销售大表-2025.xlsx', 
+    sheet_name='南京-2025-销售',
+    header=[0,1,2,3],
+)
+dfReadLeftList=dfReadLeft.values.tolist()
+with open('dfReadLeftList.json', 'w', encoding='utf-8') as f:
+    json.dump(dfReadLeftList, f, ensure_ascii=False, indent=4)
+
+dfReadRight = pd.read_excel(
+    '121 销售大表-2025.xlsx', 
+    sheet_name='25年收入凭证',
+    header=[0,1,2,3],
+)
+dfReadRightList=dfReadRight.values.tolist()
+# with open('dfReadRightList.json', 'w', encoding='utf-8') as f:
+#     json.dump(dfReadRightList, f, ensure_ascii=False, indent=4)
+
+# typeToColumnOfLeft={
+#     '单据编号':3,
+#     '金额':19,
+# }
+# leftResult={}
+# for index,item in enumerate(dfReadLeftList):
+#     currentOrderNumber=item[typeToColumnOfLeft['单据编号']]
+#     if currentOrderNumber not in leftResult:
+#         leftResult[currentOrderNumber]={
+#             'amount':0,
+#             'isna':True,
+#             'indexAll':[],
+#         }
+#     if (not pd.isna(item[typeToColumnOfLeft['金额']])):
+#         leftResult[currentOrderNumber]['amount']=leftResult[currentOrderNumber]['amount']+item[typeToColumnOfLeft['金额']]
+#         leftResult[currentOrderNumber]['isna']=False
+#     leftResult[currentOrderNumber]['indexAll'].append(index)
+
+# with open('leftResult.json', 'w', encoding='utf-8') as f:
+#     json.dump(leftResult, f, ensure_ascii=False, indent=4)
+
+
+
+def find_first_containing(lst, substring="XSCKD"):
+    for item in lst:
+        if substring in str(item):  # 确保将非字符串项转换为字符串
+            return item
+    return ''  # 未找到时返回 None
+typeToColumnOfRight={
+    '摘要':3,
+    '贷方金额':8,
+}
+rightResult={}
+for index,item in enumerate(dfReadRightList):
+    splitArray=str(item[typeToColumnOfRight['摘要']]).split('/')
+    currentOrderNumber=find_first_containing(splitArray)
+    if currentOrderNumber !='':
+        if currentOrderNumber not in rightResult :
+            rightResult[currentOrderNumber]={
+                'amount':0,
+                'isna':True,
+                'indexAll':[],
+            }
+        if (not pd.isna(item[typeToColumnOfRight['贷方金额']])):
+            rightResult[currentOrderNumber]['amount']=rightResult[currentOrderNumber]['amount']+item[typeToColumnOfRight['贷方金额']]
+            rightResult[currentOrderNumber]['isna']=False
+        rightResult[currentOrderNumber]['indexAll'].append(index)
+
+with open('rightResult.json', 'w', encoding='utf-8') as f:
+    json.dump(rightResult, f, ensure_ascii=False, indent=4)
