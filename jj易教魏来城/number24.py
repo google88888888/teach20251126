b9ad4fbff2ab7aa482ca61556290c8243568ca59
mengJiuXiang <571343428@qq.com>
2026-08-27 10:12:43 +0800

    括号

--- jj易教魏来城/number24.py
+++ jj易教魏来城/number24.py
@@ -24,65 +24,149 @@ print(picked,picked2,picked3,picked4)
 
 correctNumber=[picked,picked2,picked3,picked4]
 
+totalString=''
+
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
+
 while True:
     a = input("your solution's first number:")
     if a.isdigit() and int(a) in correctNumber:
         num2 = int(a)
+        totalString = totalString+str(num2)
         break
     else: 
         print("this can only be a number and this number should be one of ",picked,picked2,picked3,picked4)
 
 while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
+
+while True:
     b = input("+-*/:")
     if b in ("+", "-", "*", "/"):
+        totalString = totalString+str(b)
         break
     else:
         print("this can only be +,-,*,/")
         
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
 
 while True:
     c = input("your solution's second number:")
     if c.isdigit()  and int(c) in correctNumber:
         num3 = int(c)
+        totalString = totalString+str(num3)
         break
     else: 
         print("this can only be a number and this number should be one of ",picked,picked2,picked3,picked4)
 
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
 
 while True:
     d = input("+-*/:")
     if d in ("+", "-", "*", "/"):
+        totalString = totalString+str(d)
         break
     else:
         print("this can only be +,-,*,/")
 
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
 
 while True:
     e = input("your solution's third number:")
     if e.isdigit() and int(e) in correctNumber:
         num4 = int(e)
+        totalString = totalString+str(num4)
         break
     else: 
         print("this can only be a number and this number should be one of ",picked,picked2,picked3,picked4)
 
-    
+
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
+
 while True:
     f = input("+-*/:")
     if f in ("+", "-", "*", "/"):
+        totalString = totalString+str(f)
         break
     else:
         print("this can only be +,-,*,/")
 
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
 
 while True:
     g = input("your solution's fourth number:")
     if g.isdigit() and int(g) in correctNumber:
         num5 = int(g)
+        totalString = totalString+str(num5)
         break
     else: 
         print("this can only be a number and this number should be one of ",picked,picked2,picked3,picked4)
 
-totalString = str(num2)+b+str(num3)+d+str(num4)+f+str(num5)
+while True:
+    g = input("( or ) or or no:")
+    if g=='no':
+        break
+    elif g in ("(", ")"):
+        totalString = totalString+str(g)
+        break
+    else: 
+        print("this can only be ( or ) or or no")
+
 total=eval(totalString)
 
 print(total,'total')
